ebd4d3ef37253f567944b66198f2760b524b925d
Isabella Lau <izzielau@Isabellas-MacBook-Pro.local>
2018-10-13 20:21:54 -0700

    Updates to tab bar.

--- MDBFlightStatus/MDBFlightStatus/Controllers/MainTabVC.swift
+++ MDBFlightStatus/MDBFlightStatus/Controllers/MainTabVC.swift
@@ -12,25 +12,37 @@ class MainTabVC: UITabBarController {
 
     var navigation : UINavigationItem!
     
+    var datePicker : UIDatePicker!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setupTabBar()
+        setupDatePicker()
+    }
+    
+    func setupTabBar() {
         navigation = navigationItem
-        
+    
         let tab1 = FlightVC()
         let image1 = UIImage(named: "pass")
         tab1.tabBarItem = UITabBarItem(title: "Feed", image: image1, tag: 0)
-        
+    
         let tab2 = AirportVC()
         let image2 = UIImage(named: "flight")
         tab2.tabBarItem = UITabBarItem(title: "My Events", image: image2, tag: 1)
-        
+    
         let tab3 = FavoritesVC()
         let image3 = UIImage(named: "star")
         tab3.tabBarItem = UITabBarItem(title: "Favorites", image: image3, tag: 1)
-        
+    
         self.viewControllers = [tab1, tab2, tab3]
         // Do any additional setup after loading the view.
+    }
+    
+    func setupDatePicker() {
+        datePicker = UIDatePicker(frame: CGRect(x: 30, y: 3 * view.frame.height / 16, width:  view.frame.width - 60, height: view.frame.height / 8))
+        view.addSubview(datePicker)
     }
 
 }
